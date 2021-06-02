3140558a14573ba587326539867964f69ebc895e
Lisa Xu <lisaxu220@hotmail.com>
2021-06-01 22:50:02 -0400

    started setting up mapbox

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,5 +7,15 @@ class ProductsController < ApplicationController
 
   def show
     @product = Product.find(params[:id])
+    #1. call search locations method to retrieve hash of long/lat of programs
+    # @programs = 
+    #2. iterate thru array long/lat of each program
+    # @markers = @programs.geocoded.map do |program|
+    #   {
+    #     lat: program.latitude,
+    #     lng: program.longitude
+    #   }
+    # end
+    #3. display on map
   end
 end
