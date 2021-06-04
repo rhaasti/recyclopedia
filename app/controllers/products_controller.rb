e2e28d564329aa61373c83a684141f2b765f6ad0
Lisa Xu <lisaxu220@hotmail.com>
2021-06-03 22:57:40 -0400

    started geocoding

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,6 @@
 class ProductsController < ApplicationController
+  require "json"
+  require "open-uri"
 
   def index
     if params[:query].present?
@@ -10,18 +12,31 @@ class ProductsController < ApplicationController
 
   def show
     @product = Product.find(params[:id])
+
+    
     #1. call search locations method to retrieve hash of long/lat of programs
-    # @programs = 
+    @programs = get_programs
     #2. iterate thru array long/lat of each program
-    # @markers = @programs.geocoded.map do |program|
+    @markers = @programs.geocoded.map do |program|
     @markers =
       {
-        lat: 40.71,
-        lng: 74
-        # lat: program.latitude,
-        # lng: program.longitude
+        lat: program.latitude,
+        lng: program.longitude
       }
-    # end
+    end
     #3. display on map
+  end
+
+  private
+  def get_programs
+    url = "https://api.earth911.com/earth911.getPostalData?api_key=5b7412cae7282842&country=us&postal_code=#{current_user.zip_code}"
+    result_serialized = URI.open(url).read
+    result = JSON.parse(result_serialized)
+    lat = result["result"]["latitude"]
+    lng = result["result"]["longitude"]
+
+    program_url = "https://api.earth911.com/earth911.searchPrograms?api_key=5b7412cae7282842&latitude=#{lat}&longitude=#{lng}"
+    result_serialized = URI.open(program_url).read
+    result = JSON.parse(result_serialized)
   end
 end
