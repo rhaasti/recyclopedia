1521e5f012d428cf05e0ebf5c097ac79ad9950ab
Lisa Xu <lisaxu220@hotmail.com>
2021-06-05 18:11:51 -0400

    adding program details

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -1,6 +1,12 @@
 import mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
+const fitMapToMarkers = (map, markers) => {
+  const bounds = new mapboxgl.LngLatBounds();
+  markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
+  map.fitBounds(bounds, { padding: 60, maxZoom: 15, duration: 0 });
+};
+
 const initMapbox = () => {
   const mapElement = document.getElementById('map');
 
@@ -10,22 +16,31 @@ const initMapbox = () => {
       container: 'map',
       style: 'mapbox://styles/mapbox/streets-v10'
     });
-
+    
     const markers = JSON.parse(mapElement.dataset.markers);
+    // const programs = JSON.parse(mapElement.dataset.programs);
+
     markers.forEach((marker) => {
+
+      // programs.forEach((program) => {
+      var popup = new mapboxgl.Popup( { offset: 25 } )
+                  .setHTML(marker.info_window);
+      // });
+      
       new mapboxgl.Marker()
         .setLngLat([ marker.lng, marker.lat ])
+        .setPopup(popup)
         .addTo(map);
     });
 
-    fitMapToMarkers(map, markers)
+    fitMapToMarkers(map, markers);
+
   }
 };
 
-const fitMapToMarkers = (map, markers) => {
-  const bounds = new mapboxgl.LngLatBounds();
-  markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-  map.fitBounds(bounds, { padding: 60, maxZoom: 15, duration: 0 });
-};
+
+
+
+
 
 export { initMapbox };
