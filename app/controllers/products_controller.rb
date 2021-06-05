f50fb3b06479d17c5909f685f40ed96481f5effe
Lisa Xu <lisaxu220@hotmail.com>
2021-06-05 15:11:42 -0400

    embed program markers to maps

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -14,39 +14,34 @@ class ProductsController < ApplicationController
 
   def show
     @product = Product.find(params[:id])
-    
-    @zipcode = params[:zipcode] 
-    @programs = get_programs(@zipcode)
+    @material_ids = @product.material_ids
+    @zipcode = params[:zipcode]
+    @programs = get_programs(@material_ids)
     #render error page if no zipcode
-    @markers = @programs.geocoded.map do |program|
-      @markers =
+    @markers = []
+    @programs.each do |program|
+      @markers <<
         {
-          lat: program.latitude,
-          lng: program.longitude
+          lat: program["latitude"],
+          lng: program["longitude"]
         }
-      end
-    #3. display on map
+    end
   end
 
   private
-  def get_programs(zipcode)
+  def zipcode_to_coordinates(zipcode)
     url = "https://api.earth911.com/earth911.getPostalData?api_key=5b7412cae7282842&country=us&postal_code=#{zipcode}"
     result_serialized = URI.open(url).read
     result = JSON.parse(result_serialized)
-    lat = result["result"]["latitude"]
-    lng = result["result"]["longitude"]
-
-    program_url = "https://api.earth911.com/earth911.searchPrograms?api_key=5b7412cae7282842&latitude=#{lat}&longitude=#{lng}"
+    @lat = result["result"]["latitude"]
+    @lng = result["result"]["longitude"]
+  end
+  
+  def get_programs(material_ids)
+    zipcode_to_coordinates(@zipcode)
+    program_url = "https://api.earth911.com/earth911.searchPrograms?api_key=5b7412cae7282842&latitude=#{@lat}&longitude=#{@lng}&material_ids=#{material_ids}"
     result_serialized = URI.open(program_url).read
     result = JSON.parse(result_serialized)
     programs = result["result"]
-    return programs
-    # @markers = programs.geocoded.map do |program|
-    #   @markers =
-    #     {
-    #       lat: program.latitude,
-    #       lng: program.longitude
-    #     }
-    #   end
   end
 end
