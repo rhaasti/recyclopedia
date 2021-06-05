5e28432bc70e26f9cdefae0e17fa7fca1eed708f
Odette Quiroz <odette.qmon@gmail.com>
2021-06-05 15:21:27 -0500

    query for finding products by material type, added a route and a controller action search_by_material

--- db/schema.rb
+++ db/schema.rb
@@ -108,6 +108,7 @@ ActiveRecord::Schema.define(version: 2021_06_05_161120) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "first_name"
     t.string "last_name"
+    t.string "zip_code"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
