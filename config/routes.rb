d2353ba76bd1cb5dafefb14c9ae1b1c5981d22ea
Lisa Xu <lisaxu220@hotmail.com>
2021-06-06 13:30:00 -0400

    some conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -2,21 +2,15 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-resources :products, only: [:index, :show] do
-  add-lists
-  resources :bookmarks, only: [:new, :create]
+  resources :products, only: [:index, :show] do
+    collection do
+      get :search_by_material
+    end
+
+    resources :lists, only: [:index, :show, :new, :create] do
+      resources :bookmarks, only: [:new, :create]
+    end
   end
-
-
-  collection do
-    get :search_by_material
-  end
-end
-
-resources :lists, only: [:index, :show, :new, :create] do
-  resources :bookmarks, only: [:new, :create]
-end
-
 end
 
 
