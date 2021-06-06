3910d3f4a14cb68972c8afeb05df1baf546e0a8e
Ryan Hastings <rhaasti@gmail.com>
2021-06-06 14:27:03 -0400

    went through some conflicts w/ team to fix bugs

--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,12 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 resources :products, only: [:index, :show] do
-  add-lists
+     collection do
+       get :search_by_material
+    end
   resources :bookmarks, only: [:new, :create]
-  end
-
-
-  collection do
-    get :search_by_material
-  end
 end
+
 
 resources :lists, only: [:index, :show, :new, :create] do
   resources :bookmarks, only: [:new, :create]
