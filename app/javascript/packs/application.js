b678fc67a3f552065ebd5ef449c017f6ac577d2e
Lisa Xu <lisaxu220@hotmail.com>
2021-06-12 17:46:41 -0400

    close modal

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,6 +24,7 @@ require("channels")
 // External imports
 import "bootstrap";
 import { initMapbox } from './map';
+import { closeModal } from './modal';
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
@@ -31,6 +32,9 @@ import { initMapbox } from './map';
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
-  initMapbox();
+  if (document.querySelector('#map')) {
+    initMapbox();
+  };
+  closeModal();
 });
 
