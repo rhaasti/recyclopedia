067a0f468890c5f339569978d23832f8702ca1b6
Lisa Xu <lisaxu220@hotmail.com>
2021-06-17 22:28:36 -0400

    sort lists by most recent

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,7 +1,7 @@
 class ListsController < ApplicationController
 
 def index
-  @lists = List.where("user_id = #{current_user.id}")
+  @lists = List.where("user_id = #{current_user.id}").order(created_at: :desc)
 end
 
 def show
