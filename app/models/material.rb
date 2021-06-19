71f5430e048bf8f4106685adbd3315b9870ef998
Ryan Hastings <rhaasti@gmail.com>
2021-06-19 13:55:26 -0400

    passing zipcode from lists show to product show via modal now .. removed zipcode from DB .. updated model

--- app/models/material.rb
+++ app/models/material.rb
@@ -3,5 +3,4 @@ class Material < ApplicationRecord
   has_many :material_material_families
   has_many :material_families, through: :material_material_families
   has_many :products, through: :product_materials
-  has_many :programs
 end
