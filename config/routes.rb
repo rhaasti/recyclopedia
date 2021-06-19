f7bd939f8851958bbf3b76966d56ed7e795add91
Lisa Xu <lisaxu220@hotmail.com>
2021-06-19 13:23:34 -0400

    added alert to log in redirect

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,8 @@ Rails.application.routes.draw do
   resources :lists, only: [:index, :show, :new, :create] do
     resources :bookmarks, only: [:new, :create]
   end
+
+  get "/bookmarks/alert", to: "bookmarks#alert"
 end
 
 
